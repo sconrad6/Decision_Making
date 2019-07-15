2c458029e8e10bf349555977488901fe925c0884
Savanah Conrad <sibgym@yahoo.com>
2019-07-15 16:33:11 -0400

    first commit

---
+++ DecisionMaking.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Decision_Making
+{
+    class DecisionMaking
+    {
+        private readonly int userNumber;
+        private readonly string name;
+
+        public DecisionMaking()
+        {
+            userNumber = 0;
+            name = "User";
+        }
+
+        public DecisionMaking(int userNumber, string name)
+        {
+            this.userNumber = userNumber;
+            this.name = name;
+        }
+
+
+        public string WhatIsTheNumber()
+        {
+            if ((userNumber % 2 != 0) || (userNumber % 2 != 0 && userNumber > 60))
+            {
+                return userNumber + " is odd.";
+            }
+            else if (userNumber % 2 == 0 && userNumber >= 2 && userNumber <= 25)
+            {
+                return userNumber + " is even and less than 25.";
+            }
+            else if (userNumber % 2 == 0 && userNumber >= 26 && userNumber <= 60)
+            {
+                return userNumber + " is even and less than 60.";
+            }
+            else if (userNumber % 2 == 0 && userNumber > 60)
+            {
+                return userNumber + " is even.";
+            }
+            else
+            {
+                return "Sorry, " + name + ", but that number is invalid.";
+            }
+        }
+
+        public static bool IsValid(int userNumber)
+        {
+            if (userNumber < 1 || userNumber > 100)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return WhatIsTheNumber();
+            
+        }
+    }
+}
---
+++ DecisionMakingTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Decision_Making
+{
+    public class DecisionMakingTest
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! What is your name?");
+            string name = Console.ReadLine();
+
+            char userChoice;
+            do
+            {
+                Console.WriteLine("\n" + name + ", Please enter a whole number between 1 and 100");
+                int number = int.Parse(Console.ReadLine());
+                DecisionMaking decision1 = new DecisionMaking(number, name);
+
+                if (!DecisionMaking.IsValid(number))
+                {
+                    Console.WriteLine("Invalid number");
+                    return;
+                }
+              
+                Console.WriteLine("\nGreat number, " + name + "!");
+
+                Console.WriteLine(decision1);
+
+                Console.Write("\nWould you like to enter another number, " + name + "? Please press Y for yes and N for no. ");
+                userChoice = Console.ReadKey().KeyChar;
+
+            } while (userChoice == 'Y' || userChoice == 'y');
+            Console.WriteLine("\nThank you for playing, " + name + "! Have a nice day.");
+        }
+    }
+}
